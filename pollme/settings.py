65fd8de62cb6bf737f6c2c84021a9127e002c818
Mehak Aijaz <126397839+mehakaijaz@users.noreply.github.com>
2025-05-15 14:35:36 +0530

    Update settings.py

--- pollme/settings.py
+++ pollme/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d%utwhr^_^1k00o*5xilo8dvcl4kdh%a6z_15vrqma%1sz$3=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = FALSE
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
